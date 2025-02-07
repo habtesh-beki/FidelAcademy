7b01c2f09653f2376c780eeed6ef10c65e68132c
Habtemariam <habteshbaki2014@gmail.com>
2025-02-07 10:32:16 -0800

    updated some entites

--- Entites/answerIntity.ts
+++ Entites/answerIntity.ts
@@ -1,11 +1,13 @@
+import { Question } from "../models/Question.model";
 import { UserI } from "./userIntity";
 
 export interface AnswerI {
   id: string;
   body: string;
-  image: string;
-  pdf: string;
-  user: UserI;
-  question: string;
+  image?: string[];
+  pdf?: string[];
+  user?: UserI;
+  question?: Question;
   createdAt: Date;
+  updatedAt?: Date;
 }
